b10b8e0a836f9e03cc9b1171217b1b3a788e3015
davi <davimelovas61@gmail.com>
2024-01-09 16:25:54 -0300

    Adjusted some stuff

---
+++ P1/LearningWhile.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+
+int travazap = 1;
+int quantidade;
+scanf ("%d", &quantidade);
+
+while (travazap != quantidade){
+ printf("Travazap\n");
+ ++travazap;
+}
+
+return 0;
+}
