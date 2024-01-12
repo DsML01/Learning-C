6d3cdbefa21d8c53c5ae32c46ede25a33f585f29
davi <davimelovas61@gmail.com>
2024-01-12 17:40:06 -0300

    Added the binary search algorithm

--- DataStructure/SortAlgorithms/binarysearch.c
+++ DataStructure/SortAlgorithms/binarysearch.c
@@ -3,7 +3,6 @@
 #include <time.h>
 
 //TUTORIAL DE QUICK SORT FEITO PELO CANAL PROGRAME SEU FUTURO
-
 int particiona(int *v, int inicio, int fim)
 {
     //nesse caso o professor escolheu como pivot um numero que seja a media entre o primeiro valor, o valor do meio e o do final
@@ -36,6 +35,52 @@ void quicksort(int *v, int inicio, int fim)
     return;
 }
 
+void binarysearch(int *v, int start, int end, int key)
+{
+    int mid = start + (end - start) / 2;
+
+    if(start <= end)
+    {
+        if(v[mid] == key)
+        {
+            puts("The number exists and was found!");
+            printf("%d\n", key);
+        }
+
+        else if(key > v[mid])
+        {
+            binarysearch(v, mid + 1, end, key);
+        }
+        else
+        {
+            binarysearch(v, start, mid - 1, key);
+        }
+    }
+    else
+    {
+        if(v[mid] == key)
+        {
+            puts("The number exists and was found!");
+            printf("%d\n", key);
+        }
+        else
+            puts("The number does not exist and was not found!");
+    }
+
+    return;
+}
+
+void bs(int *v, int n)
+{
+    int key;
+    puts("Which number do you want to find?");
+    scanf("%d", &key);
+
+    binarysearch(v, 0, n - 1, key);
+
+    return;
+}
+
 void print(int *v, int tam)
 {
     int i;
@@ -50,7 +95,7 @@ void randomize(int v[], int n)
 
     for(int i = 0; i <= n; i++)
     {
-        v[i] = rand()/100000000;
+        v[i] = rand()/100000;
     }
 
     return;
@@ -58,7 +103,7 @@ void randomize(int v[], int n)
 
 int main()
 {
-    int n = 100000000;
+    int n = 1000;
 
     //array de teste
     //v[n] = {47, 35, 12, 49, 56, 25, 71, 83, 5, 3};
@@ -69,10 +114,14 @@ int main()
 
     //Printa o array antes de fazer o sort
     print(v, n - 1);
+    puts("\n\n\n\n\n");
     
     quicksort(v, 0, n - 1);
     
     print(v, n - 1);
+    puts("\n\n\n\n\n");
+
+    bs(v, n);
 
     return 0;
 }
